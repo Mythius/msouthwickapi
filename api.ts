684f29ededa5381cdd431d981fea7c9f5f16d113
Matthias Southwick <southwickmatthias@gmail.com>
2026-09-02 12:36:46 -0600

    Update api.ts

--- api.ts
+++ api.ts
@@ -45,7 +45,7 @@ export function publicRoutes(app: Hono): void {
       body: JSON.stringify({
         model: "gemma2:2b",
         stream: false,
-        format: "json",
+        format: "text",
         prompt: decodedText
       })
     })
